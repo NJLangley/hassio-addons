92b7530520a1a6dfeed1317884ace19bf17c99fa
Niall <niall.langley@gmail.com>
2019-09-17 14:52:00 +0100

    Fixes for jq errors

--- letsdnsocloud/functions.sh
+++ letsdnsocloud/functions.sh
@@ -4,13 +4,13 @@
 function grabzoneid() {
 
   #Strip Subdomain to get bare domain
-  BASEDOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< $LE_DOMAINS)
+  BASEDOMAIN=$(sed 's/ /\n/g' <<< $(sed 's/.*\.\(.*\..*\)/\1/' <<< $LE_DOMAINS) | uniq)
 
   #Grab Zoneid & Export for Hooks.sh
   export ZONEID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones" \
     -H "X-Auth-Email: $CF_EMAIL" \
     -H "X-Auth-Key: $CF_APIKEY" \
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$BASEDOMAIN'"))) | .id')
+    -H "Content-Type: application/json" | jq -r --arg BASEDOMAIN "$BASEDOMAIN" '.result[] | (select(.name | contains($BASEDOMAIN))) | .id')
 }
 
 #Grab id from existing A record
@@ -19,32 +19,40 @@ function grabaid() {
   AID=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records" \
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
-    -H "Content-Type: application/json" | jq -r '.result[] | (select(.name | contains("'$LE_DOMAINS'"))) | (select (.type | contains("A"))) | .id')
+    -H "Content-Type: application/json" | jq -r --arg BASEDOMAIN "$BASEDOMAIN"'.result[] | (select(.name | contains($BASEDOMAIN))) | (select (.type | contains("A"))) | .id')
 
 }
 
 #Create A record
 function createarecord() {
-
+echo "Creating A record for $BASEDOMAIN at $IP"
   curl -sX POST "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records"\
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
     -H "Content-Type: application/json"\
-    --data '{"type":"A","name":"'$LE_DOMAINS'","content":"'$IP'","proxied":false}' -o /dev/null
+    --data '{"type":"A","name":"'$BASEDOMAIN'","content":"'$IP'","proxied":false}' -o /dev/null
 
-echo "A record created for $LE_DOMAINS at $IP"
+echo "A record created for $BASEDOMAIN at $IP"
 
 }
 
 #Update A record IP address
 function updateip() {
+  echo "Updating $BASEDOMAIN with IP: $1"
+
+  echo "curl -sX PUT \"https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$AID\"\
+    -H \"X-Auth-Email: $CF_EMAIL\"\
+    -H \"X-Auth-Key: $CF_APIKEY\"\
+    -H \"Content-Type: application/json\"\
+    --data '{\"type\":\"A\",\"name\":\"'$BASEDOMAIN'\",\"content\":\"'$1'\",\"proxied\":false}'" #-o /dev/null
+
 
   curl -sX PUT "https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$AID"\
     -H "X-Auth-Email: $CF_EMAIL"\
     -H "X-Auth-Key: $CF_APIKEY"\
     -H "Content-Type: application/json"\
-    --data '{"type":"A","name":"'$LE_DOMAINS'","content":"'$1'","proxied":false}' -o /dev/null
+    --data '{"type":"A","name":"'$BASEDOMAIN'","content":"'$1'","proxied":false}' #-o /dev/null
 
-  echo "Updated $LE_DOMAINS with IP: $1"
+  echo "Updated $BASEDOMAIN with IP: $1"
 
 }
--- letsdnsocloud/run.sh
+++ letsdnsocloud/run.sh
@@ -21,21 +21,26 @@ WAIT_TIME=$(jq --raw-output '.seconds' $CONFIG_PATH)
 
 #Extract Zone ID for Domain
 grabzoneid
+
 #Exract A record ID if one exists already
 grabaid
 
 #Grab current ip
 IP=$(curl -s "https://ipinfo.io/ip")
 
+echo "1"
 #Create A Record or update existing with current IP
 if [ -z "$AID" ]
   then
+    echo "1A"
     createarecord
   else
+    echo "1B"
     updateip $IP
 fi
 
 # Register/generate certificate if terms accepted
+echo "2"
 
 function le_renew() {
     local domain_args=()
@@ -48,12 +53,15 @@ function le_renew() {
 }
 
 if [ "$LE_TERMS" == "true" ]; then
+    echo "2A"
+    
     # Init folder structs
     mkdir -p "$CERT_DIR"
     mkdir -p "$WORK_DIR"
 
     # Generate new certs
     if [ ! -d "$CERT_DIR/live" ]; then
+        echo "2B"
         # Create empty dehydrated config file so that this dir will be used for storage
         touch "$WORK_DIR/config"
         dehydrated --register --accept-terms --config "$WORK_DIR/config"
@@ -62,16 +70,21 @@ fi
 
 # Loop: Watch for new IP and update. Renew Cert after 30 days
 while true; do
-
+    echo "Checking IP..."
     NEWIP=$(curl -s "https://ipinfo.io/ip")
 
     if [ "$IP" != "$NEWIP" ]; then
+      echo "Updating IP..."
       updateip $NEWIP
+      echo "Updated IP"
     fi
 
+    echo "Checking if certificate needs updating..."
     now="$(date +%s)"
     if [ "$LE_TERMS" == "true" ] && [ $((now - LE_UPDATE)) -ge 43200 ]; then
+        echo "Updating certificate..."
         le_renew
+        echo "Updated certificate..."
     fi
     sleep "$WAIT_TIME"
 done
